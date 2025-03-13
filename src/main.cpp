61c5a425e0c749f3aa5bcada1b3c7ecd29b3a309
KingsDev <63374258+KingsMMA@users.noreply.github.com>
2025-03-13 18:33:09 +1000

    Created window

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,29 @@
 #include <Windows.h>
+#include <dwmapi.h>
+#include <d3d11.h>
+
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_dx11.h>
+#include <imgui/imgui_impl_win32.h>
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+/**
+* The window procedure receives the messages being sent to the window
+* This is just like hey imgui, do you want to handle?  If not, pass the message along
+*/
+LRESULT CALLBACK window_procedure(HWND window, UINT message, WPARAM w_param, LPARAM l_param) {
+	if (ImGui_ImplWin32_WndProcHandler(window, message, w_param, l_param)) {
+		return 0L;
+	}
+
+	if (message == WM_DESTROY) {
+		PostQuitMessage(0);
+		return 0L;
+	}
+
+	return DefWindowProc(window, message, w_param, l_param);
+}
 
 /**
 * @parameter instance This window's instance
@@ -7,5 +32,140 @@
 * @parameter cmd_show Determines whether or not we want to show our window
 */
 INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
+	// Create the window class
+	WNDCLASSEXW wc{};
+	wc.cbSize = sizeof(WNDCLASSEXW);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = window_procedure;
+	wc.hInstance = instance;
+	wc.lpszClassName = L"Whiteboard Class";
+
+	// Register the window class
+	RegisterClassExW(&wc);
+
+	// Use the window class to create a window
+	const HWND window = CreateWindowExW(
+		WS_EX_TOPMOST | WS_EX_TRANSPARENT | WS_EX_LAYERED,
+		wc.lpszClassName,
+		L"Whiteboard",
+		WS_POPUP,
+		0,
+		0,
+		2560, // width - look into using GetSystemMetrics or smth
+		1440, // height
+		nullptr,
+		nullptr,
+		wc.hInstance,
+		nullptr
+	);
+	
+	SetLayeredWindowAttributes(window, RGB(0, 0, 0), BYTE(255), LWA_ALPHA);  // The BYTE(255) is the alpha value.
+
+	// This is needed to make stuff render
+	{
+		RECT client_area{};
+		GetClientRect(window, &client_area);
+
+		RECT window_area{};
+		GetWindowRect(window, &window_area);
+
+		POINT diff{};
+		ClientToScreen(window, &diff);
+
+		const MARGINS margins{
+			window_area.left + (diff.x - window_area.left),
+			window_area.top + (diff.y - window_area.top),
+			client_area.right,
+			client_area.bottom
+		};
+
+		DwmExtendFrameIntoClientArea(window, &margins);
+	}
+
+	DXGI_SWAP_CHAIN_DESC sd{};
+	sd.BufferDesc.RefreshRate.Numerator = 144U;
+	sd.BufferDesc.RefreshRate.Denominator = 1U;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.SampleDesc.Count = 1U;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.BufferCount = 2U;
+	sd.OutputWindow = window;
+	sd.Windowed = TRUE;
+	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+	// Compile time array containg the DX11 features that we want
+	constexpr D3D_FEATURE_LEVEL levels[2]{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_0,
+	};
+
+	ID3D11Device* device{ nullptr };
+	ID3D11DeviceContext* device_context{ nullptr };
+	IDXGISwapChain* swap_chain{ nullptr };
+	ID3D11RenderTargetView* render_target_view{ nullptr };
+	D3D_FEATURE_LEVEL level{};
+
+	D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		0U,
+		levels,
+		2U,
+		D3D11_SDK_VERSION,
+		&sd,
+		&swap_chain,
+		&device,
+		&level,
+		&device_context
+	);
+
+	// Create the render target
+	ID3D11Texture2D* back_buffer{ nullptr };
+	swap_chain->GetBuffer(0U, IID_PPV_ARGS(&back_buffer));
+
+	if (back_buffer) {
+		device->CreateRenderTargetView(back_buffer, nullptr, &render_target_view);
+		back_buffer->Release();
+	} else return 1;
+
+	ShowWindow(window, cmd_show);
+	UpdateWindow(window);
+
+	ImGui::CreateContext();
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplWin32_Init(window);
+	ImGui_ImplDX11_Init(device, device_context);
+
+	bool running = true;
+	while (running) {
+		MSG msg;
+		while (PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+			if (msg.message == WM_QUIT) {
+				running = false;
+			}
+		}
+		if (!running) break;
+	}
+
+	// Close the program, cleanup
+	ImGui_ImplDX11_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+
+	ImGui::DestroyContext();
+
+	if (swap_chain) swap_chain->Release();
+	if (device_context) device_context->Release();
+	if (device) device->Release();
+	if (render_target_view) render_target_view->Release();
+
+	DestroyWindow(window);
+	UnregisterClassW(wc.lpszClassName, wc.hInstance);
+
 	return 0;
 }
