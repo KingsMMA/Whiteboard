610ad2eea3da3a64d422991ebf018f53549eb104
KingsDev <63374258+KingsMMA@users.noreply.github.com>
2025-03-13 17:11:37 +1000

    Changed some settings + added entrypoint
    Credit to https://www.youtube.com/watch?v=aY8MNCNN-cY for the tutorial on setting up an overlay project

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Windows.h>
+
+/**
+* @parameter instance This window's instance
+* @parameter _ The previous window's instance
+* @parameter _ The command line arguments
+* @parameter cmd_show Determines whether or not we want to show our window
+*/
+INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
+	return 0;
+}
